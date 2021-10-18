7a470f165ccdeaa304e62908559d23827e717d5c
Jason Lengstorf <jason@lengstorf.com>
2021-10-18 10:33:39 -0700

    wip: get Hasura and Apollo wired up

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { useQuery, gql } from '@apollo/client';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data } = useQuery(gql`
+    query LoadQuestions {
+      questions {
+        id
+        question
+        answers {
+          id
+          text
+          count
+        }
+      }
+    }
+  `);
+
+  if (loading) {
+    return <p>loading...</p>;
+  }
+
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {data.questions &&
+        data.questions.map((question) => (
+          <div key={`question-${question.id}`}>
+            <h3>{question.question}</h3>
+            {question.answers.map((answer) => (
+              <button key={`answer-${answer.id}`}>
+                {answer.text} ({answer.count})
+              </button>
+            ))}
+          </div>
+        ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://ato-vite-hasura-apollo.hasura.app/v1/graphql',
+  cache: new InMemoryCache(),
+});
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
